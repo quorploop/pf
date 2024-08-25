05559aa8d7f5188e6312af77f3aac527348f600b
Linus Silberstein <l.silberstein@ostfalia.de>
2024-08-25 19:49:27 +0200

    Adds new Collection

--- src/content/config.js
+++ src/content/config.js
@@ -2,16 +2,27 @@
 import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
 const projectCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        tags: z.array(z.string()).optional(),
-        description: z.string(),
-        date: z.date().optional()
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    description: z.string(),
+    date: z.date().optional()
+  })
 });
+
+const jobcollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    description: z.string(),
+    date: z.date().optional()
+  })
+})
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'project': projectCollection
+  'project': projectCollection,
+  'job': jobcollection
 };
---
+++ src/content/job/volkswagen.md
@@ -0,0 +1,10 @@
+---
+title: Volkswagen Group
+description: Work at Volkswagen Group, Germany (work study)
+date: 2021-08-01
+link: 'https://www.volkswagen.de/de.html'
+tags: ['work', 'study', 'data analyst']
+---
+
+I worked at Volkswagen Group, Germany for roughly three and a half years.
+During that time VW supported my bachelors degree.
