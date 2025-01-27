9f41942f426e0cc44317a86e43dce4eca3de6d6a
Linus Silberstein <l.silberstein@ostfalia.de>
2025-01-27 17:29:35 +0100

    Adds Posts collection type

--- src/content/config.js
+++ src/content/config.js
@@ -20,9 +20,21 @@ const jobcollection = defineCollection({
     date: z.date().optional()
   })
 })
+
+const postCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    description: z.string(),
+    date: z.date().optional()
+  })
+})
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'project': projectCollection,
-  'job': jobcollection
+  'job': jobcollection, 
+  'post': postCollection
 };
