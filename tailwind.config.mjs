69b7af8409e86f844d06a24fbfc51c60941ec995
Linus Silberstein <l.silberstein@ostfalia.de>
2025-02-01 11:40:01 +0100

    Fixes Issue with Marquee

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,12 +21,17 @@ export default {
 				mono: ['Reddit Mono', 'mono']
 			},
 			animation: {
-				marquee: 'marquee 30s linear infinite'
+				marquee: 'marquee 30s linear infinite',
+				marquee2: 'marquee 30s linear infinite'
 			},
 			keyframes: {
 				marquee: {
 					'0%': { transform: 'translateX(0)' },
 					'100%': { transform: 'translateX(-100%)' }
+				},
+				marquee2: {
+					'0%': { transform: 'translateX(100%)' },
+					'100%': { transform: 'translateX(0)' }
 				}
 			}
 		}
